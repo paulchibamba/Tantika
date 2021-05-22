21912baf67fa7d420bd7de448cde1637d5083fdf
paulchibamba <61985521+paulchibamba@users.noreply.github.com>
2021-05-23 00:19:36 +0200

    Inserting data into database working.
    Added a shared viewModel to the fragments package

--- app/src/main/java/com/paulchibamba/tantika/fragments/add/AddFragment.kt
+++ app/src/main/java/com/paulchibamba/tantika/fragments/add/AddFragment.kt
@@ -1,15 +1,32 @@
 package com.paulchibamba.tantika.fragments.add
 
 import android.os.Bundle
+import android.renderscript.RenderScript
 import android.text.TextUtils
 import android.view.*
 import android.widget.EditText
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.paulchibamba.tantika.R
+import com.paulchibamba.tantika.data.ToDoDatabase
+import com.paulchibamba.tantika.data.models.Priority
+import com.paulchibamba.tantika.data.models.ToDoData
+import com.paulchibamba.tantika.data.viewmodel.ToDoViewModel
+import com.paulchibamba.tantika.fragments.SharedViewModel
+import kotlinx.coroutines.InternalCoroutinesApi
 
 
+@InternalCoroutinesApi
 class AddFragment : Fragment() {
+    @InternalCoroutinesApi
+    private val mTodoViewModel: ToDoViewModel by viewModels()
+    private val mSharedViewModel: SharedViewModel by viewModels()
+    private lateinit var titleEditText: EditText
+    private lateinit var prioritySpinner: Spinner
+    private lateinit var descriptionEditText: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,16 +35,12 @@ class AddFragment : Fragment() {
         //Set Menu
         setHasOptionsMenu(true)
 
-
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val mTitle = view.findViewById<EditText>(R.id.title_et).text.toString()
-        val mPriority = view.findViewById<Spinner>(R.id.priorities_spinner).selectedItem.toString()
-        val mDescription = view.findViewById<EditText>(R.id.description_et).text.toString()
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+        titleEditText = view.findViewById(R.id.title_et) as EditText
+        prioritySpinner = view.findViewById(R.id.priorities_spinner) as Spinner
+        descriptionEditText = view.findViewById(R.id.description_et) as EditText
+        return view
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -41,15 +54,30 @@ class AddFragment : Fragment() {
         return super.onOptionsItemSelected(item)
     }
 
-    private fun insertDataToDb(view: View) {
-        val mTitle = view.findViewById<EditText>(R.id.title_et).text.toString()
-        val mDescription = view.findViewById<EditText>(R.id.description_et).text.toString()
-        val validation = verifyDataFromUser(mTitle, mDescription)
+    @InternalCoroutinesApi
+    private fun insertDataToDb() {
+        val mTitle = titleEditText.text.toString()
+        val mPriority = prioritySpinner.selectedItem.toString()
+        val mDescription = descriptionEditText.text.toString()
+        val validation = mSharedViewModel.verifyDataFromUser(mTitle, mDescription)
+
+        if (validation){
+            //Insert Data into DB
+            val newData = ToDoData(
+                    0,
+                    mTitle,
+                    mSharedViewModel.parsePriority(mPriority),
+                    mDescription
+            )
+            mTodoViewModel.insertData(newData)
+            Toast.makeText(requireContext(), "Successfully Added!", Toast.LENGTH_SHORT).show()
+
+            //Navigate back to List fragment
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        } else {
+            Toast.makeText(requireContext(), "Please Fill Out All Fields", Toast.LENGTH_SHORT).show()
+        }
     }
 
-    private fun verifyDataFromUser(title: String, description: String): Boolean{
-        return if (TextUtils.isEmpty(title) || TextUtils.isEmpty(description)) {
-            false
-        }else !(title.isEmpty() || description.isEmpty())
-    }
+
 }
